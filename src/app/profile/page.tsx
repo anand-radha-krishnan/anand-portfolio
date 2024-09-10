1e53cb97538756fad03e52657d72d1cae4b604d8
anand-radha-krishnan <anandradhakrishna@outlook.com>
2024-09-10 11:31:25 +0200

    test commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import Spline from "@splinetool/react-spline";
 export default function Profile() {
   return (
     <div>
+      helllo
       <Spline scene="https://prod.spline.design/FVZWbQH2B6ndj9UU/scene.splinecode" />
     </div>
   );
