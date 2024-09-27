cff7a5d9725953e400f5041770c03e834f618297
anand-radha-krishnan <anandradhakrishna@outlook.com>
2024-09-27 09:47:10 +0200

    portfolio: alignment fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Spline from "@splinetool/react-spline";
 
 export default function Contact() {
-  return <div>welcome to contact page</div>;
+  return <div>Coming soon...</div>;
 }
