c36f449f597314310f914bae3216214c0651bc3d
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-02-14 10:32:42 +0100

    add theming feature

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,8 +3,8 @@ import Experience from "@/components/Experience";
 export default function Profile() {
   return (
     <div className="m-4 md:m-0">
-      <div className="mx-auto max-w-xl text-2xl font-bold mt-24 mb-10">
-        Work Experience
+      <div className="mx-auto max-w-xl text-2xl font-bold mt-24 mb-10 text-center">
+        Experience
       </div>
       <Experience />
     </div>
