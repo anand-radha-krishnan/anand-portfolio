3910d848fd9ce8f2674b2f8b1fb0e568a2897629
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-02-14 15:48:07 +0100

    adding env variables

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   apiVersion: "2025-02-14",
   useCdn: true,
 });
