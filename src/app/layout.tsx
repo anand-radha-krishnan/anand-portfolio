dd9fd98a7087c4bce0238526a58ef8e3cc1a779b
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-02-26 18:20:05 +0100

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ThemeProvider } from "@/components/provider/ThemeProvider";
 // import Header from "@/components/Header";
@@ -39,6 +40,7 @@ export default function RootLayout({
           </QueryProvider>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
